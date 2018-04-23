440a496a58a88862de5635f88dec81ed5d9785cb
Dmytro Pechenyi <dmytro_pechenyi@epam.com>
2018-04-23 14:58:46 +0300

    page not foung

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
-
+import logo from '../styles/logo.svg';
+import {Navbar, Nav, NavItem} from 'react-bootstrap';
 import '../styles/App.css';
 
 
 const NotFound = () => (
-  <div className="centered-context">
-    <h1>Page not found</h1>
-  </div>
+        <div className="App">
+            <Navbar>
+                <Nav className="flex-row p-3 form-control">
+                    <NavItem className="navbar-toggler " eventKey={1} href="/">Todo Tasks</NavItem>
+                    <NavItem className="navbar-toggler" eventKey={2} href="/done">Done Tasks</NavItem>
+                    <NavItem className="navbar-toggler" eventKey={3} href="/archived">Archived Tasks</NavItem>
+                </Nav>
+            </Navbar>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <h1 className="App-title">Page Not Found</h1>
+            </header>
+        </div>
 );
 
 export default NotFound;
