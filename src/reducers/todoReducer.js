2c40eb370e97e4c8e9d8d52e2a057ed42a9f5746
petrro_gordeichuk <petro_gordeichuk@epam.com>
2018-04-24 22:15:56 +0300

    Working with small issues

--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -4,24 +4,22 @@ import * as types from '../utils/constants';
 const initialState = new InitialState();
 
 const todoReducer = (state = initialState, action) => {
-  // if (!(state instanceof InitialState)) state = initialState.mergeDeep(state);
 
   switch(action.type) {
     case types.SET_TODO_DONE_SUCCESS:
       return state.set('todos', state.todos.map((todo) => todo.id === action.payload.id ? { ...todo, done: action.payload.done } : todo));
 
     case types.SET_TODO_ARCHIVED_SUCCESS:
-    {
-      console.log("action.payload.archived :" + action.payload.archived);
-      return state.set('todos', state.todos.map((todo) => todo.id === action.payload.id ? { ...todo, archived: action.payload.archived } : todo)
-        .filter((todo) => todo.id !== action.payload.id));
-    }
+      return state.set('todos', state.todos.map((todo) => todo.id === action.payload.id ? { ...todo, archived: action.payload.archived } : todo));
+
     case types.DELETE_TODO_SUCCESS:
       return state.set('todos', state.todos.filter((todo) => todo.id !== action.payload.id));
     
     case types.ADD_TODO_SUCCESS:
-      // return state.set('todos', [ ...state.todos, { id: getLastId(state.todos) + 1, task: action.payload.task, done: false } ]);
       return state.set('todos', [...state.todos, action.payload.todo]);
+
+      case types.EDIT_TODO_SUCCESS:
+      return state.set('todos', state.todos.map((todo) => todo.id === action.payload.id ? { ...todo, task: action.payload.task } : todo));
 
     case types.FETCH_TODOS_SUCCESS:
       return state.set('todos', [...action.payload.todos]);
