90066e6225d58a72b4f109f794ffc9407201c4b4
Dmytro Pechenyi <dmytro_pechenyi@epam.com>
2018-04-24 17:13:53 +0300

    working container

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,50 +1,66 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {Button, ListGroupItem} from 'react-bootstrap';
 
-const Todo = ({todo, setDone, deleteTodo, archiveTodo, editTodo}) => (
-    <ListGroupItem className="list-group-item list-group-item-action d-flex w-90 h=90 justify-content-between">
-        <h4 className="mb-1">{todo.task}</h4>
-        <a>
-            {!todo.archived ?
-                <Button type="button" className="btn btn-success" onClick={(e) => {
-                    e.preventDefault();
-                    setDone(todo, !todo.done)
-                }}>{todo.done ? 'Mark Undone' : 'Mark Done'}</Button> : ''
-            }
+export class Todo extends Component {
 
-            <Button type="button" className="btn btn btn-secondary" onClick={(e) => {
-                e.preventDefault();
-                archiveTodo(todo, !todo.archived)
-            }}> {!todo.archived ? 'Archive' : 'UnArchive'}</Button>
+    static propTypes = {
+        todo: PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            task: PropTypes.string.isRequired,
+            done: PropTypes.bool.isRequired,
+            deleted: PropTypes.bool.isRequired,
+            archived: PropTypes.bool.isRequired
+        }).isRequired,
+        setDone: PropTypes.func.isRequired,
+        deleteTodo: PropTypes.func.isRequired,
+        archiveTodo: PropTypes.func.isRequired,
+    };
 
-            {!todo.archived ?
-                <Button type="button" className="btn btn-danger" onClick={(e) => {
-                    e.preventDefault();
-                    deleteTodo(todo.id)
-                }}>Delete</Button> : ''
-            }
-            {!todo.archived ?
-                <Button type="button" className="btn btn-info" onClick={(e) => {
-                    e.preventDefault();
-                    editTodo(todo.id)
-                }}>Edit</Button> : ''
-            }
-        </a>
-    </ListGroupItem>
-);
+    constructor(props) {
+        super(props);
+        console.info(props);
+        this.state = {
+            todo: props.todo,
+            setDone: props.setDone,
+            deleteTodo: props.deleteTodo,
+            archiveTodo: props.archiveTodo
+        };
+    }
 
-Todo.propTypes = {
-    todo: PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        task: PropTypes.string.isRequired,
-        done: PropTypes.bool.isRequired,
-        deleted: PropTypes.bool.isRequired,
-        archived: PropTypes.bool.isRequired
-    }).isRequired,
-    setDone: PropTypes.func.isRequired,
-    deleteTodo: PropTypes.func.isRequired,
-    archiveTodo: PropTypes.func.isRequired,
-};
+    render() {
+        return (
+            <ListGroupItem className="list-group-item list-group-item-action d-flex w-90 h=90 justify-content-between">
+                <h4 className="mb-1">{this.state.todo.task}</h4>
+                <a>
+                    {!this.state.todo.archived ?
+                        <Button type="button" className="btn btn-success" onClick={(e) => {
+                            e.preventDefault();
+                            this.state.setDone(this.state.todo, !this.state.todo.done)
+                        }}>{this.state.todo.done ? 'Mark Undone' : 'Mark Done'}</Button> : ''
+                    }
+
+                    <Button type="button" className="btn btn btn-secondary" onClick={(e) => {
+                        e.preventDefault();
+                        this.state.archiveTodo(this.state.todo, !this.state.todo.archived)
+                    }}> {!this.state.todo.archived ? 'Archive' : 'UnArchive'}</Button>
+
+                    {!this.state.todo.archived ?
+                        <Button type="button" className="btn btn-danger" onClick={(e) => {
+                            e.preventDefault();
+                            this.state.deleteTodo(this.state.todo.id)
+                        }}>Delete</Button> : ''
+                    }
+                    {!this.state.todo.archived ?
+                        <Button type="button" className="btn btn-info" onClick={(e) => {
+                            e.preventDefault();
+                            this.state.editTodo(this.state.todo.id)
+                        }}>Edit</Button> : ''
+                    }
+                </a>
+            </ListGroupItem>
+        )
+    }
+}
 
 export default Todo;
