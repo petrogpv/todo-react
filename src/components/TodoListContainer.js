e3e70807875210e416dc37819c0899b436bd729d
Dmytro Pechenyi <dmytro_pechenyi@epam.com>
2018-04-24 15:11:58 +0300

    remove todoSelector

--- src/components/TodoListContainer.js
+++ src/components/TodoListContainer.js
@@ -1,55 +1,53 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
 
-import { setTodoDone, deleteTodo, archiveTodo, addTodo, editTodo, searchFilter, fetchTodos } from '../utils/todoActions';
-import { getVisibleTodos } from '../utils/todoSelectors';
+import {setTodoDone, deleteTodo, archiveTodo, addTodo, editTodo, searchFilter, fetchTodos} from '../utils/todoActions';
 import TodoList from './TodoList';
 
 
 export class TodoListContainer extends Component {
 
-  static propTypes = {
-    todos: PropTypes.arrayOf(PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      task: PropTypes.string.isRequired,
-      done: PropTypes.bool.isRequired,
-      deleted: PropTypes.bool.isRequired,
-      archived: PropTypes.bool.isRequired,
-    })).isRequired,
-    setTodoDone: PropTypes.func.isRequired,
-    deleteTodo: PropTypes.func.isRequired,
-    archiveTodo: PropTypes.func.isRequired,
-    addTodo: PropTypes.func.isRequired,
-    editTodo: PropTypes.func.isRequired,
-    searchFilter: PropTypes.func.isRequired,
-    fetchTodos: PropTypes.func.isRequired
-  };
+    static propTypes = {
+        todos: PropTypes.arrayOf(PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            task: PropTypes.string.isRequired,
+            done: PropTypes.bool.isRequired,
+            deleted: PropTypes.bool.isRequired,
+            archived: PropTypes.bool.isRequired,
+        })).isRequired,
+        setTodoDone: PropTypes.func.isRequired,
+        deleteTodo: PropTypes.func.isRequired,
+        archiveTodo: PropTypes.func.isRequired,
+        addTodo: PropTypes.func.isRequired,
+        editTodo: PropTypes.func.isRequired,
+        searchFilter: PropTypes.func.isRequired,
+        fetchTodos: PropTypes.func.isRequired
+    };
 
-  componentDidMount() {
-    this.props.fetchTodos();
-  }
+    componentDidMount() {
+        this.props.fetchTodos();
+    }
 
-  render() {
-    return <TodoList {...this.props} />
-  }
-
+    render() {
+        return <TodoList {...this.props} />
+    }
 }
 
 const mapStateToProps = state => ({
-  todos: getVisibleTodos(state),
+    todos: state.todoReducer.todos,
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators({
-  setTodoDone,
-  deleteTodo,
-  archiveTodo,
-  addTodo,
-  editTodo,
-  searchFilter,
-  fetchTodos,
+    setTodoDone,
+    deleteTodo,
+    archiveTodo,
+    addTodo,
+    editTodo,
+    searchFilter,
+    fetchTodos,
 }, dispatch);
 
 
